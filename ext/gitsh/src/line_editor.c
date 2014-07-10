a06e7f26305300bf4221fdddde36a934f59f3ff5
George Brocklehurst <george@georgebrock.com>
2014-07-10 13:43:17 +0200

    Require readline in the C extension.
    This means the reference to the Ruby readline module needs to be renamed,
    because it clashes with the readline function.

--- ext/gitsh/src/line_editor.c
+++ ext/gitsh/src/line_editor.c
@@ -1,6 +1,8 @@
 #include "ruby.h"
 
-VALUE readline = Qnil;
+#include <readline/readline.h>
+
+VALUE m_readline = Qnil;
 VALUE gitsh = Qnil;
 VALUE line_editor = Qnil;
 
@@ -17,8 +19,8 @@ void
 Init_line_editor()
 {
     rb_require("readline");
+    m_readline = rb_const_get(rb_cObject, rb_intern("Readline"));
 
-    readline = rb_const_get(rb_cObject, rb_intern("Readline"));
     gitsh = rb_define_module("Gitsh");
     line_editor = rb_define_module_under(gitsh, "LineEditor");
 
@@ -43,49 +45,49 @@ Init_line_editor()
 VALUE
 line_editor_history(VALUE module)
 {
-    return rb_const_get(readline, rb_intern("HISTORY"));
+    return rb_const_get(m_readline, rb_intern("HISTORY"));
 }
 
 VALUE
 line_editor_set_completion_append_character(VALUE module, VALUE append_chr)
 {
-    return rb_funcall(readline, rb_intern("completion_append_character="), 1,
+    return rb_funcall(m_readline, rb_intern("completion_append_character="), 1,
         append_chr);
 }
 
 VALUE
 line_editor_set_completion_proc(VALUE module, VALUE proc)
 {
-    return rb_funcall(readline, rb_intern("completion_proc="), 1, proc);
+    return rb_funcall(m_readline, rb_intern("completion_proc="), 1, proc);
 }
 
 VALUE
 line_editor_set_input(VALUE module, VALUE input)
 {
-    return rb_funcall(readline, rb_intern("input="), 1, input);
+    return rb_funcall(m_readline, rb_intern("input="), 1, input);
 }
 
 VALUE
 line_editor_set_output(VALUE module, VALUE output)
 {
-    return rb_funcall(readline, rb_intern("output="), 1, output);
+    return rb_funcall(m_readline, rb_intern("output="), 1, output);
 }
 
 VALUE
 line_editor_readline(VALUE module, VALUE prompt, VALUE add_to_history)
 {
-    return rb_funcall(readline, rb_intern("readline"), 2,
+    return rb_funcall(m_readline, rb_intern("readline"), 2,
         prompt, add_to_history);
 }
 
 VALUE
 line_editor_line_buffer(VALUE module)
 {
-    return rb_funcall(readline, rb_intern("line_buffer"), 0);
+    return rb_funcall(m_readline, rb_intern("line_buffer"), 0);
 }
 
 VALUE
 line_editor_emacs_editing_mode(VALUE module)
 {
-    return rb_funcall(readline, rb_intern("emacs_editing_mode"), 0);
+    return rb_funcall(m_readline, rb_intern("emacs_editing_mode"), 0);
 }
